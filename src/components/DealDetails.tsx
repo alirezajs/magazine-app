e8e4b355a375b57796d14315ade21eccb4b25512
alirezajs <alireza.varmagahani@gmail.com>
2022-03-09 11:10:18 +0300

    Make the style of the detail a little betther

--- src/components/DealDetails.tsx
+++ src/components/DealDetails.tsx
@@ -30,7 +30,7 @@ class DealDetails extends Component<DealItemProps, DealItemState> {
     const deal = this.state.deal;
     return (
       <View style={styles.deal}>
-        <TouchableOpacity onPress={this.props.onBack}>
+        <TouchableOpacity onPress={this.props.onBack} style={styles.backButton}>
           <Text>Back</Text>
         </TouchableOpacity>
         <Image
@@ -41,23 +41,26 @@ class DealDetails extends Component<DealItemProps, DealItemState> {
         />
         <View style={styles.info}>
           <Text style={styles.title}>{deal.title}</Text>
+        </View>
+        <View style={styles.footer}>
           <View>
             <Text style={styles.cause}>{priceDisplay(deal.price)}</Text>
             <Text style={styles.price}>{deal.cause.name}</Text>
           </View>
+
+          {deal.user && (
+            <View>
+              <Image
+                style={styles.avatar}
+                source={{
+                  uri: deal.user.avatar,
+                }}
+              />
+              <Text>{deal.user.name}</Text>
+            </View>
+          )}
         </View>
-        {deal.user && (
-          <View>
-            <Image
-              style={styles.avatar}
-              source={{
-                uri: deal.user.avatar,
-              }}
-            />
-            <Text>{deal.user.name}</Text>
-          </View>
-        )}
-        <View>
+        <View style={styles.description}>
           <Text>{deal.description}</Text>
         </View>
       </View>
@@ -67,33 +70,36 @@ class DealDetails extends Component<DealItemProps, DealItemState> {
 
 const styles = StyleSheet.create({
   deal: {
-    marginHorizontal: 12,
-    borderColor: "#bbb",
-    borderWidth: 1,
+    marginHorizontal: 0,
+  },
+  backButton: {
+    color: "#0645ad",
+    marginBottom: 10,
+    marginLeft: 10,
   },
   image: {
     width: "100%",
-    height: 150,
+    height: 200,
     backgroundColor: "#ccc",
   },
   info: {
     padding: 10,
-    backgroundColor: "#fff",
-    borderColor: "rgb(187 184 184)",
-    borderWidth: 1,
-    borderTopWidth: 0,
+    backgroundColor: "rgb(255,113,0)",
+    color: "#fff",
   },
   title: {
     fontSize: 16,
     fontWeight: "bold",
     marginBottom: 5,
     padding: 10,
+    color: "#fff",
   },
   footer: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
     marginTop: 15,
+    padding: 10,
   },
   cause: {
     flex: 2,
@@ -103,8 +109,15 @@ const styles = StyleSheet.create({
     textAlign: "right",
   },
   avatar: {
-    width: "30%",
+    width: 50,
     height: 50,
+    borderRadius: 200,
+  },
+  description: {
+    borderColor: "#eee",
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
   },
 });
 
